3fa4a065d2cf21d0a8f7af1f4e19664f88ac215c
Maria Veronica Baca Alaniz (24152) <maria.baca@est.zamorano.edu>
2022-09-28 19:50:54 -0600

    Version 3

---
+++ Lab04/Ejercicio7.py
@@ -0,0 +1,12 @@
+while True:
+    numero = input("Ingrese un número: ")
+    if (numero == "salir"):
+        break
+    if (numero == 1):
+        print ("uno")
+    elif (numero == 2):
+        print ("dos")
+    elif (numero == 3):
+        print("tres")
+    else:
+        print("No definido")
